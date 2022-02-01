9fdaaca5a11ed2c343921d624a746efb776a7527
rajon-ahmed <raazon.ahmed1@gmail.com>
2022-02-02 01:12:11 +0600

    lets have a look in ck editor code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import MyEditor from "./MyEditor";
 
-function App() {
+export default function App(props) {
+  const [editor, setEditor] = useState(null);
+  console.log('from state', editor)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MyEditor
+        handleChange={(data) => {
+          setEditor(data);
+        }}
+        data={editor}
+        {...props}
+      />
+    </>
   );
 }
-
-export default App;
